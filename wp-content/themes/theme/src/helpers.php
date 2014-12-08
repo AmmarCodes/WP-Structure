5019a2e0cc2e807d2b1b647e057aad5c5f6fd5a7
Ammar Alakkad <am.alakkad@gmail.com>
2014-12-08 12:12:22 +0200

    Theme - Helpers: add function to parse manifest.json and give appropriate file

--- wp-content/themes/theme/src/helpers.php
+++ wp-content/themes/theme/src/helpers.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * Debug and die.
+ *
+ * @param  object $param variable
+ */
 function dd($param)
 {
     echo '<pre dir="ltr">';
@@ -11,3 +16,25 @@ function dd($param)
     die();
 }
 
+/**
+ * Get asset file based on revisioned (hashed) file map (assets/manifest.json)
+ *
+ * @param  string $filename asset file name
+ * @return string           hashed name for the asset file if key exists in manifest.json or the exact file if key doesn't exist.
+ */
+function asset($filename) {
+    $assets = trailingslashit(get_template_directory_uri()) . 'assets/';
+    $manifest_path = $assets . 'manifest.json';
+
+    if (file_exists($manifest_path)) {
+        $manifest = json_decode(file_get_contents($manifest_path), true);
+    } else {
+        $manifest = [];
+    }
+
+    if (array_key_exists($filename, $manifest)) {
+        return $assets . $manifest[$filename];
+    }
+
+    return $assets . $filename;
+}
