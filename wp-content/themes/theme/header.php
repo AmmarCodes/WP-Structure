cb181a6740fdca3282115a3accd718540fd63137
Ammar Alakkad <am.alakkad@gmail.com>
2014-12-08 13:09:44 +0200

    Theme - Header: cleaning up, add IE conditional scripts

--- wp-content/themes/theme/header.php
+++ wp-content/themes/theme/header.php
@@ -2,15 +2,13 @@
 <html class="no-js" lang="ar" dir="rtl">
 <head>
     <meta charset="utf-8">
-    <meta name="description" content="">
+    <meta name="description" content="<?php bloginfo('description');?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, minimal-ui">
     <title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
-
-    <!-- <link rel="stylesheet" href="../bower_components/fontawesome/css/font-awesome.css" /> -->
-
-    <!--
-    <script src="../bower_components/modernizr/modernizr.js"></script>
-     -->
+    <!--[if lt IE 9]>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7/html5shiv.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.3.0/respond.js"></script>
+    <![endif]-->
     <?php wp_head();?>
 </head>
 <body>
